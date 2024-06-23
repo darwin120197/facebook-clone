570aace0698c1b17ae5d61d864f906a59023f591
Darwin Morante Lopez <158999828+darwin120197@users.noreply.github.com>
2024-06-23 16:46:25 +0800

    Add files via upload

---
+++ main.js
@@ -0,0 +1,21 @@
+const moreList = document.querySelectorAll(".see-more-list"),
+  seeMoreBtn = document.getElementById("see-more-btn"),
+  seeLessBtn = document.getElementById("see-less-btn");
+
+document.addEventListener("DOMContentLoaded", () => {
+  seeMoreBtn.addEventListener("click", () => {
+    moreList.forEach((e) => {
+      e.classList.add("display-block");
+    });
+
+    seeMoreBtn.classList.add("display-none");
+  });
+
+  seeLessBtn.addEventListener("click", () => {
+    moreList.forEach((e) => {
+      e.classList.remove("display-block");
+    });
+
+    seeMoreBtn.classList.remove("display-none");
+  });
+});
